c9e556f07cb60544cb01f90b78f4d0a1bcc60c91
Aziz <sha.milov@mail.ru>
2026-02-21 11:04:14 +0400

    Exercise 7

---
+++ EXERCISE7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EXERCISE7 {
+
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String reversed = reverseString(text);
+        System.out.println("Reversed: " + reversed);
+
+        sc.close();
+    }
+}
